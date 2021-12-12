bed9396b1120d4ac72a145fbb331054aa1719fb8
nothing2512 <blank345red@gmail.com>
2021-12-12 20:44:04 +0700

    (+) Fixing

--- app/Http/Controllers/UKMController.php
+++ app/Http/Controllers/UKMController.php
@@ -2,19 +2,27 @@
 
 namespace App\Http\Controllers;
 
+use App\Helpers\Services;
+
 class UKMController extends Controller
 {
     public function ormawa()
     {
+        $data = Services::getUkm(TAB_ORMAWA);
         return view('list-ukm', [
-            "title" => "List UKM"
+            "title" => "List UKM",
+            "data" => $data['data'],
+            "subTitle" => "Ormawa"
         ]);
     }
 
     public function opmawa()
     {
+        $data = Services::getUkm(TAB_OPMAWA);
         return view('list-ukm', [
-            "title" => "List UKM"
+            "title" => "List UKM",
+            "data" => $data['data'],
+            "subTitle" => "Opmawa"
         ]);
     }
 }
